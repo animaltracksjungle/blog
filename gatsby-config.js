c164ea863d527ba23a4ec6bb8826b2e0f397a1a3
Animal Tracks <animaltracksjungle@gmail.com>
2023-06-28 14:34:48 -0600

    Google Analytics + sec ups.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
   },
   plugins: [
+
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -35,6 +36,23 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+     resolve: `gatsby-plugin-google-gtag`,
+     options: {
+       // You can add multiple tracking ids and a pageview event will be fired for all of them.
+       trackingIds: [
+         "G-T3TMJPQV3S", // Google Analytics / GA
+       ],
+       // This object is used for configuration specific to this plugin
+       pluginConfig: {
+         // Puts tracking script in the head instead of the body
+         head: true,
+         // Setting this parameter is also optional
+         respectDNT: true,
+         // Avoids sending pageview hits from custom paths
+         // exclude: ["/preview/**", "/do-not-track/me/too/"],
+       },
     },
     {
       resolve: `gatsby-transformer-remark`,
